5eadf94dbff0ab1a625f4f3884cd829c2bf3aa38
Ibn-Ahmed <ahmedibn19@yahoo.com>
2022-10-18 01:11:15 +0000

    recent commit

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 const burger = document.getElementById("burger");
 const burgerMenu = document.getElementById("nav-links");
 const hamburger = document.getElementById("hamburger");
+const wrapper = document.getElementById("wrapper");
 
+
+//open and close menu, toggle menu and close sign
 burger.addEventListener('click',()=> {
     burgerMenu.classList.toggle("nav-active");
     hamburger.classList.toggle("toggle");
     burgerMenu.classList.toggle("none");
+
 })
 
+//click anywhere to close menu
 document.onclick = function(e) {
     if(e.target.id !='nav-links' && e.target.id !='hamburger') {
         burgerMenu.classList.remove('nav-active')
@@ -15,3 +20,21 @@ document.onclick = function(e) {
             burgerMenu.classList.add('none')
     }
 }
+
+
+
+//when scrolling has stopped.
+const timer = -1;
+document.addEventListener('scroll', function() {
+    wrapper.style.backgroundColor = "#FFF";
+    wrapper.style.boxShadow = "4px 0 84px 20px rgb(77 90 203 / 17%)";
+    if(timer !== -1) {
+        clearTimeout(timer);        
+    }
+    timer = setTimeout(function() {
+          // do something
+
+        wrapper.style.backgroundColor = "#EBEFF6";
+           
+    }, 250);
+}, true);
